b2cf42d07bc93e36cb5904936126a2e3ee29c431
Seth Epps <seth.d.epps@gmail.com>
2024-02-28 15:07:51 -0500

    Remove variant concept from native template generation

--- scratch/templates/embedded.go
+++ scratch/templates/embedded.go
@@ -20,23 +20,18 @@ func NewEmbeddedTemplateProvider(language, variant string) EmbeddedTemplateProvi
 }
 
 func (tp EmbeddedTemplateProvider) Get() ([]Template, error) {
-	if variants, ok := SupportedTemplates[tp.language]; ok {
-
-		if !slices.Contains(variants, tp.variant) {
-			return nil, fmt.Errorf("%v variant is not supported for %v", tp.variant, tp.language)
-		}
-		return findLanguageVariant(tp.language, tp.variant)
-	} else {
-		return nil, fmt.Errorf("%v is not supported natively", tp.language)
+	if !slices.Contains(SupportedTemplates, tp.language) {
+		return nil, fmt.Errorf("%v is not supported", tp.language)
 	}
+	return collectTemplates(tp.language)
 }
 
 func (tp EmbeddedTemplateProvider) TargetPath() string {
 	return filepath.Join(tp.language, tp.variant)
 }
 
-func findLanguageVariant(language, variant string) ([]Template, error) {
-	embeddedPath := filepath.Join(templateFSRoot, language, variant)
+func collectTemplates(language string) ([]Template, error) {
+	embeddedPath := filepath.Join(templateFSRoot, language)
 	pathGlob := filepath.Join(embeddedPath, "*.tpl")
 
 	tmplGlob := template.Must(
@@ -46,7 +41,6 @@ func findLanguageVariant(language, variant string) ([]Template, error) {
 	)
 
 	var templates []Template
-
 	for _, tmpl := range tmplGlob.Templates() {
 		relPath, _ := strings.CutSuffix(tmpl.Name(), ".tpl")
 		templates = append(templates, Template{
@@ -56,7 +50,7 @@ func findLanguageVariant(language, variant string) ([]Template, error) {
 	}
 
 	if len(templates) == 0 {
-		return nil, fmt.Errorf("Could not find native templates for `%v (%v)`", language, variant)
+		return nil, fmt.Errorf("could not find native templates for `%v`", language)
 	}
 
 	return templates, nil
